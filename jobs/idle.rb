9c7a7f4b3b15e9df1fdeafeb09c2722dc5322c35
William Clifford <wobh@yahoo.com>
2013-04-19 14:55:12 -0700

    FIXED: setup 'idle.rb' and 'lib/graphite.rb' to use URI composition

--- jobs/idle.rb
+++ jobs/idle.rb
@@ -1,17 +1,22 @@
 require "./lib/graphite"
+require 'URI'
 
 # last started parkingsessions
 SCHEDULER.every '30s', :first_in => 0 do
-    # Create an instance of our helper class
-    q = Graphite.new "http://localhost:8080/graphite"
+  # Create an instance of our helper class
+  q = Graphite.new URI::Generic.build(:scheme => 'http',
+                                      :host   => 'localhost',
+                                      :port   => 9999,
+                                      :path   => '/graphite')
 
-    graphite_test_target = "graphite.com.crowdcompass.vagrant.graphite.cpu.0.idle"
-
-    # get the current value
-    current = q.value graphite_test_target, "-1min"
-    # get points for the last half hour
-    # points = q.points graphite_test_target, "-30min"
-
-    # send to dashboard, so the number the meter and the graph widget can understand it
-    send_event 'graphite_test', { current: current }
+  graphite_test_target = 'graphite.com.crowdcompass.vagrant.graphite.cpu.0.idle'
+  
+  # get the current value
+  current = q.value(graphite_test_target, '-1min')
+  # get points for the last half hour
+  # points = q.points(graphite_test_target, "-30min")
+  
+  # send to dashboard, so the number the meter and the graph widget
+  # can understand it
+  send_event('graphite_test', { current: current })
 end
