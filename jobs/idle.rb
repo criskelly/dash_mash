7aa087178c720d316050f4cafe6d512815ef0271
Cris Kelly <cris.kelly@gmail.com>
2013-04-22 17:03:16 -0700

    added threshold number in coffeescript file

--- jobs/idle.rb
+++ jobs/idle.rb
@@ -2,17 +2,17 @@ require "./lib/graphite"
 require './lib/data_server_conf'
 
 # last started parkingsessions
-SCHEDULER.every '30s', :first_in => 0 do
+SCHEDULER.every '2s', :first_in => 0 do
   # Create an instance of our helper class
   q = Graphite.new DataServerConf::GRAPHITE
 
   graphite_test_target = 'graphite.com.crowdcompass.vagrant.graphite.cpu.0.idle'
-  
+
   # get the current value
   current = q.value(graphite_test_target, '-1min')
   # get points for the last half hour
   # points = q.points(graphite_test_target, "-30min")
-  
+
   # send to dashboard, so the number the meter and the graph widget
   # can understand it
   send_event('graphite_test', { current: current })
---
+++ widgets/number_sandbox/number_sandbox.coffee
@@ -0,0 +1,20 @@
+class Dashing.Number_Sandbox extends Dashing.Widget
+  @accessor 'current', Dashing.AnimatedValue
+
+  @accessor 'difference', ->
+    if @get('last')
+      last = parseInt(@get('last'))
+      current = parseInt(@get('current'))
+      if last != 0
+        diff = Math.abs(Math.round((current - last) / last * 100))
+        "#{diff}%"
+    else
+      ""
+
+  @accessor 'arrow', ->
+    if @get('last')
+      if parseInt(@get('current')) > parseInt(@get('last')) then 'icon-arrow-up' else 'icon-arrow-down'
+
+  onData: (data) ->
+    if data.current < 96.0
+      $(@node).css('background-color','blue')
