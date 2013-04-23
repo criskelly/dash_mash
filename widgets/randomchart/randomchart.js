dcc2ccb456ea89c5ef026b46134a9271e3497401
Cris Kelly <cris.kelly@gmail.com>
2013-04-22 17:04:20 -0700

    deleted randomchart

--- widgets/randomchart/randomchart.js
+++
@@ -1,7 +0,0 @@
-var graph = new Rickshaw.Graph({
-    series: [ { data: [ { x: 0, y: 2 }, { x: 1, y: 4 } ] } ],
-    renderer: 'area',
-    element: document.querySelector('#graph')
-});
-
-graph.render();
